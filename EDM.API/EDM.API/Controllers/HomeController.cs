e5acfa7dd0e7f3e3578cf265d578d3ca34df1ccb
Pragnesh Makwana <info@webdroids.in>
2020-04-22 00:53:16 +0530

    update

---
+++ EDM.API/EDM.API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EDM.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
