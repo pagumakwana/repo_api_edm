a39b184446aa307f48426b25fdac53d510704e3a
Pragnesh Makwana <info@webdroids.in>
2020-06-22 00:57:51 +0530

    update

--- EDM.API/EDM.DataAccessLayer/User/ClsUser_DAL.cs
+++ EDM.API/EDM.DataAccessLayer/User/ClsUser_DAL.cs
@@ -17,36 +17,32 @@ namespace EDM.DataAccessLayer.User
                 DBParameterCollection ObJParameterCOl = new DBParameterCollection();
                 DBParameter objDBParameter = new DBParameter("@Ref_User_ID", ObjUser.Ref_User_ID, DbType.Int64);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@User_Code", ObjUser.User_Code, DbType.String);
+                objDBParameter = new DBParameter("@FullName", ObjUser.FullName, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@User_FirstName", ObjUser.User_FirstName, DbType.String);
+                objDBParameter = new DBParameter("@EmailID", ObjUser.EmailID, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@User_LastName", ObjUser.User_LastName, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@User_EmailID", ObjUser.User_EmailID, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@User_Password", ObjUser.User_Password, DbType.String);
+                objDBParameter = new DBParameter("@Password", ObjUser.Password, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
                 objDBParameter = new DBParameter("@Profile_Photo", ObjUser.Profile_Photo, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@Bio", ObjUser.Bio, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@Gender", ObjUser.Gender, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@Pincode", ObjUser.Pincode, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@Address", ObjUser.Address, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@Address1", ObjUser.Address1, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@AuthorityIDs", ObjUser.AuthorityIDs, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
                 objDBParameter = new DBParameter("@UserMasterDataIDs", ObjUser.UserMasterDataIDs, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@IsActive", ObjUser.IsActive, DbType.Boolean);
-                ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@CreatedBy", ObjUser.CreatedBy, DbType.String);
-                ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@Bio", ObjUser.Bio, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@Gender", ObjUser.Gender, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@Pincode", ObjUser.Pincode, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@Address", ObjUser.Address, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@Address1", ObjUser.Address1, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@AuthorityIDs", ObjUser.AuthorityIDs, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@IsActive", ObjUser.IsActive, DbType.Boolean);
+                //ObJParameterCOl.Add(objDBParameter);
+                //objDBParameter = new DBParameter("@CreatedBy", ObjUser.CreatedBy, DbType.String);
+                //ObJParameterCOl.Add(objDBParameter);
 
                 DBHelper objDbHelper = new DBHelper();
                 return Convert.ToString(objDbHelper.ExecuteScalar("[DBO].[SignUp]", ObJParameterCOl, CommandType.StoredProcedure));
@@ -64,7 +60,7 @@ namespace EDM.DataAccessLayer.User
                 DBParameterCollection ObJParameterCOl = new DBParameterCollection();
                 DBParameter objDBParameter = new DBParameter("@User_Code", ObjUser.User_Code, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
-                objDBParameter = new DBParameter("@password", ObjUser.Password, DbType.String);
+                objDBParameter = new DBParameter("@Password", ObjUser.Password, DbType.String);
                 ObJParameterCOl.Add(objDBParameter);
 
                 DBHelper objDbHelper = new DBHelper();
@@ -80,13 +76,12 @@ namespace EDM.DataAccessLayer.User
                             {
                                 Ref_User_ID = Row.Field<Int64>("Ref_User_ID"),
                                 User_Code = Row.Field<string>("User_Code"),
-                                User_FirstName = Row.Field<string>("User_FirstName"),
-                                User_LastName = Row.Field<string>("User_LastName"),
-                                User_EmailID = Row.Field<string>("User_EmailID"),
+                                FullName = Row.Field<string>("FullName"),
+                                EmailID = Row.Field<string>("EmailID"),
                                 Profile_Photo = Row.Field<string>("Profile_Photo"),
                                 Bio = Row.Field<string>("Bio"),
                                 Gender = Row.Field<string>("Gender"),
-                                User_Password = Row.Field<string>("User_Password"),
+                                Password = Row.Field<string>("Password"),
                                 Pincode = Row.Field<Int64>("Pincode"),
                                 Address = Row.Field<string>("Address"),
                                 Address1 = Row.Field<string>("Address1"),
--- EDM.API/EDM.Models/User/clsUser_BO.cs
+++ EDM.API/EDM.Models/User/clsUser_BO.cs
@@ -10,13 +10,13 @@ namespace EDM.Models.User
     {
         public Int64 Ref_User_ID { get; set; }
         public string User_Code { get; set; }
-        public string User_FirstName { get; set; }
+        public string FullName { get; set; }
         public string User_LastName { get; set; }
-        public string User_EmailID { get; set; }
+        public string EmailID { get; set; }
         public string Profile_Photo { get; set; }
         public string Bio { get; set; }
         public string Gender { get; set; }
-        public string User_Password { get; set; }
+        public string Password { get; set; }
         public Int64 Pincode { get; set; }
         public string Address { get; set; }
         public string Address1 { get; set; }
