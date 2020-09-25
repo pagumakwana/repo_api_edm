9559049263a626a924c63fece8db52d5f8ed9bf1
rawshan-more <roshanmore2905@gmail.com>
2020-09-25 16:01:46 +0530

    update

--- EDM.API/EDM.API/Controllers/User/TrackController.cs
+++ EDM.API/EDM.API/Controllers/User/TrackController.cs
@@ -13,7 +13,7 @@ namespace EDM.API.Controllers.User
     {
         [Route("FeaturedTrack")]
         [HttpGet]
-        public List<ClsFeaturedTrack> GetFeaturedTrackList(Int64 UserID, int StartCount, int EndCount)
+        public List<ClsFeaturedTrack> GetFeaturedTrackList(Int64 UserID = 0, int StartCount = 0, int EndCount = 0)
         {
             using (ClsTrack_BAL obj = new ClsTrack_BAL())
             {
